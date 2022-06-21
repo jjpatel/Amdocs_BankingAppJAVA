76c8b5d21ef5994e279d412e929f8a0084b07ff0
jjpatel <67829946+jjpatel@users.noreply.github.com>
2022-06-21 09:30:06 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -4,36 +4,48 @@ import java.util.*;
 public class Bank {
 
 	String accNo;
-	String name;
+	String fname;
+	String lname;
+	String address;
+	String accType;
 	float balance;
 	
 	Scanner s = new Scanner(System.in);
 	
 	void openAccount() {
-		System.out.println("Enter Account No: ");
+		System.out.println("\nEnter Account No: ");
 		accNo = s.nextLine();
-		System.out.println("Enter Name: ");
-		name = s.nextLine();
+		System.out.println("Enter First Name: ");
+		fname = s.nextLine();
+		System.out.println("Enter Last Name: ");
+		lname = s.nextLine();
+		System.out.println("Enter Address: ");
+		address = s.nextLine();
+		System.out.println("Enter Account Type: ");
+		accType = s.nextLine();
 		System.out.println("Enter Balance (Minimum Rs.1000): ");
 		balance = s.nextFloat();		
 	}
 	
 	void displayAccount() {
-		System.out.println("Account No : "+ accNo);
-		System.out.println("Name : "+ name);
-		System.out.println("Balance : "+ balance);		
+		System.out.println("\nAccount No : "+ accNo);
+		System.out.println("Name : "+ fname + " " + lname);
+		System.out.println("Address : "+ address);
+		System.out.println("Account Type : "+ accType);
+		System.out.println("Balance : "+ balance);
 	}
 	
 	void deposit() {
 		float amount;
-		System.out.println("Enter the amount to be deposited: ");
+		System.out.println("\nEnter the amount to be deposited: ");
 		amount = s.nextFloat();
 		balance += amount;
+		System.out.println("Transaction Type: Deposit, Amount = "+amount + ", Balance = "+ balance);
 	}
 	
 	void withdraw() {
 		float amount;
-		System.out.println("Enter the amount to be withdrawn: ");
+		System.out.println("\nEnter the amount to be withdrawn: ");
 		amount = s.nextFloat();
 		if(amount > balance) {
 			System.out.println("Insufficient Balance! "+ "Current Balance = "+ balance);			
@@ -43,6 +55,7 @@ public class Bank {
 		}
 		else {
 			balance -= amount;
+			System.out.println("Transaction Type: Withdraw"+ amount+", Balance = "+ balance);
 		}
 	}
 	
